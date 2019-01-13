6ea367ee1419244c03bb9b4fe23bfde1ba4900b9
wray <wray@wrayesian.com>
2019-01-13 18:21:47 -0500

    clearly don't know difference between pelicanconf and publishconf

--- publishconf.py
+++ publishconf.py
@@ -10,8 +10,8 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://blog.techemstudios.com'
-RELATIVE_URLS = False
+#SITEURL = 'http://blog.techemstudios.com'
+#RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
