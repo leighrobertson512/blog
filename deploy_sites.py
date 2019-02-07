dba11c6e1455f95edf024bf64d79fe0c601eddd8
Matthew <sirtomato999@gmail.com>
2019-02-07 15:53:22 -0500

    Update deploy_sites.py

--- deploy_sites.py
+++ deploy_sites.py
@@ -3,8 +3,16 @@ import os
 branches_raw = os.system('git branch -a').split("\n")
 
 for branch in branches_raw[1:]:
-  branch = branch.lstrip('  origin/')
+  branch = branch.lstrip("  origin/")
+  print("Branch: " + branch)
+  
+  print    ("make github DOMAIN=" + branch "+.techemstudios.com")
   os.system("make github DOMAIN=" + branch "+.techemstudios.com")
+  
+  print    ("git remote remove origin")
   os.system("git remote remove origin")
-  os.system("git remote add origin https://" + os.environ('GH_TOKEN') + "@github.com/techemstudios/" + branch)
+  
+  print    ("git remote add origin https://" + os.environ("GH_TOKEN") + "@github.com/techemstudios/" + branch)
+  os.system("git remote add origin https://" + os.environ("GH_TOKEN") + "@github.com/techemstudios/" + branch)
+  
   os.system("git push -f origin gh-pages")
